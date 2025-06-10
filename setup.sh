df2fd95a75146d59128a9a63692cbcf7a9865e43
Naim Azizi <naim.azizi@traveloka.com>
2025-06-10 12:23:03 +0700

    update

--- setup.sh
+++ setup.sh
@@ -2,19 +2,21 @@
 export XDG_CONFIG_HOME="$HOME"/.config
 mkdir -p "$XDG_CONFIG_HOME"
 
+git submodule update
+
 ln -sf "$PWD/.config/nvim" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$PWD/.config/fish" "$XDG_CONFIG_HOME/fish"
 
 packages=(
-  fd
-  ripgrep
-  gitui
-  fish
+	fd
+	ripgrep
+	gitui
+	fish
 )
 
 for package in "${packages[@]}"; do
-  echo "Installing $package..."
-  /home/linuxbrew/.linuxbrew/bin/brew install "$package"
+	echo "Installing $package..."
+	/home/linuxbrew/.linuxbrew/bin/brew install "$package"
 done
 
 echo "All packages installed successfully."
