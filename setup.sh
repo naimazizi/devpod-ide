8b8c32204e0f9eb34880947dfbd546015f6807cf
Naim Azizi <naim.azizi@traveloka.com>
2025-06-10 15:09:18 +0700

    update homebrew

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 packages=(
 	build-essential
-	fd-find
-	ripgrep
 	fish
 	cargo
+)
+
+brew_packages=(
+	pyenv
+	pyenv-virtualenv
+	lsd
+	neovim
+	gitui
+	fd
+	ripgrep
+	fzf
 )
 
 sudo apt update -y
@@ -13,8 +22,12 @@ for package in "${packages[@]}"; do
 	sudo apt install -y "$package"
 done
 
-curl -fsSL https://pyenv.run | bash
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+
+for package in "${brew_packages[@]}"; do
+	echo "Installing $package..."
+	/home/linuxbrew/.linuxbrew/bin/brew install "$package"
+done
 
 mkdir -p ~/.config
 cd ~/.config
